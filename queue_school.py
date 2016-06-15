4b2fb49a1a47716be9c5ec055e94772f9a2cdc21
Khatri <ckhatri@LAMU02RH4EMG8WP.uhc.com>
2016-06-15 15:46:34 -0400

    finished queue school

--- queue_school.py
+++ queue_school.py
@@ -2,16 +2,20 @@
 
 numKids, numSeconds = map(int, raw_input().split(" "))
 originalLine = raw_input()
+#create an array, False = girls, True = Boys, goes through and sets correctly
 originalBools = [False] * numKids
 posInArray = 0
 for char in originalLine:
 	if char == "B":
 		originalBools[posInArray] = True
 	posInArray += 1
+# for number seconds
 for num in range(numSeconds):
+	#check to see if you've already flipped, if so don't do anything
 	flip = False
 	for num in range(0, numKids-1):
 		if not flip:
+			#if a boy, check if next is a girl, if it is then flip
 			if originalBools[num]:
 				if not originalBools[num + 1]:
 					originalBools[num] = False
@@ -23,7 +27,9 @@ for num in range(numSeconds):
 				flip = False
 		else:
 			flip = False
+
 finalLine = ""
+#rewrite
 for person in originalBools:
 	if person:
 		finalLine += "B"
