91aef3cde68bb421af9437b8636cd9a1e9ef0de0
Khatri <ckhatri@LAMU02RH4EMG8WP.uhc.com>
2016-06-21 08:36:28 -0400

    finished expression.py

--- expression.py
+++ expression.py
@@ -1,3 +1,5 @@
+# http://codeforces.com/problemset/problem/479/A
+
 def addOrMult(a, b):
 	if a == 1 or b == 1:
 		return a + b
@@ -8,6 +10,12 @@ a = int(input())
 b = int(input())
 c = int(input())
 
+#can do op between a and b first, or b and c first.
 d = addOrMult(a, b)
-result = addOrMult(c, d)
-print result
+f = addOrMult(b, c)
+result1 = addOrMult(c, d)
+result3 = addOrMult(a, f)
+if result1 >= result3:
+	print result1
+else:
+	print result3
