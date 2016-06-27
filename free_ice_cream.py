5ef3305a398e60b4c73bf818d18278fdd5d77958
Khatri <ckhatri@LAMU02RH4EMG8WP.uhc.com>
2016-06-27 08:53:05 -0400

    FREEEE ice cream yay

---
+++ free_ice_cream.py
@@ -0,0 +1,15 @@
+numInputs, numIcecream = map(int, raw_input().split(" "))
+numDistressed = 0
+for numInputs in xrange(0, numInputs):
+	stringInput = map(str, raw_input().split(" "))
+	sign = stringInput[0]
+	amount = int(stringInput[1])
+	if sign == "+":
+		numIcecream += amount
+	else:
+		if amount > numIcecream:
+			numDistressed += 1
+		else:
+			numIcecream -= amount
+print "%d %d" % (numIcecream, numDistressed)
+
